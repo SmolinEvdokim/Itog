0c7ab4920e63cdaa3df1885c82cbca9e4273f1c6
SmolinEvdokim <smolinevdok@gmail.com>
2023-02-12 18:11:56 +0100

    Osnovnoi zikl

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿ Console.WriteLine("VVedite massiv strok cheres probel");
+﻿ string[] Mass;
+ Console.WriteLine("VVedite massiv strok cheres probel");
+ string stroka = Console.ReadLine();
+            Mass = stroka.Split(' ');
+            var result = new string[Mass .Length];
+            var realSize = 0;
+            foreach (var value in Mass )
+            {
+                if (value.Length <= 3)
+                {
+                    result[realSize] = value;
+                    realSize++;
+                }
+            }
